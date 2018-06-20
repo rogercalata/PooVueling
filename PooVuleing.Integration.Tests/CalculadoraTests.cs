2520c2e19c282b247efbaf78c8944c4ca50c6d4a
rogercalata <35268126+rogercalata@users.noreply.github.com>
2018-06-20 15:35:13 +0200

    Subida del codigo
    subida del codigo fuente

---
+++ PooVuleing.Integration.Tests/CalculadoraTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PooVuleing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooVuleing.Tests
+{
+    [TestClass()]
+    public class CalculadoraTests
+    {
+        [TestMethod()]
+        public void CloneTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiplicacionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RestaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SumaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
