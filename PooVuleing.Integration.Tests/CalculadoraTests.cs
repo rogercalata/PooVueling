7b2e14a7d8607e4d270d7d7bc2cafad005a3fe7d
rogercalata <35268126+rogercalata@users.noreply.github.com>
2018-06-21 16:33:11 +0200

    Integration.Test
    subida del archivo de testing

--- PooVuleing.Integration.Tests/CalculadoraTests.cs
+++ PooVuleing.Integration.Tests/CalculadoraTests.cs
@@ -10,30 +10,38 @@ namespace PooVuleing.Tests
 {
     [TestClass()]
     public class CalculadoraTests
-    { 
-
-        [TestMethod()]
-        public void DivisionTest()
+    {
+        ICalculadora iCalculadora = new Calculadora();
+        [DataRow(4, 2, 2)]
+        [DataRow(9, 3, 3)]
+        [DataTestMethod]
+        public void DivisionTest(int num1, int num2, int resultado)
         {
-            Assert.Fail();
+            Assert.IsTrue(iCalculadora.Division(num1, num2) == resultado);
         }
 
-        [TestMethod()]
-        public void MultiplicacionTest()
+        [DataRow(4, 2, 8)]
+        [DataRow(6, 3, 18)]
+        [DataTestMethod]
+        public void MultiplicacionTest(int num1, int num2, int resultado)
         {
-            Assert.Fail();
+            Assert.IsTrue(iCalculadora.Multiplicacion(num1, num2) == resultado);
         }
 
-        [TestMethod()]
-        public void RestaTest()
+        [DataRow(4, 2, 2)]
+        [DataRow(6, 3, 3)]
+        [DataTestMethod]
+        public void RestaTest(int num1, int num2, int resultado)
         {
-            Assert.Fail();
+            Assert.IsTrue(iCalculadora.Resta(num1, num2) == resultado);
         }
 
-        [TestMethod()]
-        public void SumaTest()
+        [DataRow(2, 2, 4)]
+        [DataRow(6, 3, 9)]
+        [DataTestMethod]
+        public void SumaTest(int num1, int num2, int resultado)
         {
-            Assert.Fail();
+            Assert.IsTrue(iCalculadora.Suma(num1, num2) == resultado);
         }
     }
 }
